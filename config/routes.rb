0138e56aefea7629b63d0e6faac9eed9238594b8
jeremycartesse <cartessejeremy@gmail.com>
2017-02-21 15:36:08 +0100

    simplification master

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,11 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
 
-  get 'dashboard/show'
-
-  get 'dashboard/new'
-
-  get 'dashboard/create'
-
-  get 'dashboard/edit'
-
-  get 'dashboard/delete'
-
-  get 'dashboard/update'
-
-  resources :users do
-    resources :offers
+  resources :offers
+  devise_for :users
+  resources :users, only: [] do
+    get '/offers', to: "users#offers"
   end
 
-  devise_for :users
   root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
