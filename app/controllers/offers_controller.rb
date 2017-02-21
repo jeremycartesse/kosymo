5701a2fb65300b08bd6c2d0f125e25108e810d37
Yonni <yonnibibas@gmail.com>
2017-02-21 13:54:03 +0100

    configuration par défaut de offers_controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,22 +1,42 @@
 class OffersController < ApplicationController
   def index
+    @offers = Offer.all
   end
 
   def show
+    @offer = Offer.find(params[:id])
   end
 
   def new
+    @offer = Offer.new
   end
 
   def create
+    @offer = Offer.new(task_params)
+    @offer.save
+    redirect_to ????_path
   end
 
   def edit
+    @offer = Offer.find(params[:id])
   end
 
   def update
+    @offer = Offer.find(params[:id])
+    @offer.update(task_params)
+    redirect_to ????_path
   end
 
   def destroy
+    @offer = Offer.find(params[:id])
+    @offer.destroy
+    redirect_to ????_path
   end
+
+  private
+
+  def task_params
+    params.require(:offer).permit(:name, :address, :city, :zip_code, :price, :description, :room_number, :surface, :type)
+  end
+
 end
