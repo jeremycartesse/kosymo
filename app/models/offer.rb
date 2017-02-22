8f8377a4c574fdd5a350b4db531e32626b7e87e1
jeremycartesse <cartessejeremy@gmail.com>
2017-02-22 15:04:03 +0100

    offer_options

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,8 +1,8 @@
 class Offer < ApplicationRecord
   ROOM_NUMBER = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
   BEDROOM_NUMBER = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-  TYPE = ["Appartment", "Floor", "House", "Building", "Commercial premises", "Parking"]
-  belongs_to :user_id
+  GOOD_TYPE = ["Appartment", "Floor", "House", "Building", "Commercial premises", "Parking"]
+  belongs_to :user
   validates :name, presence: true
   validates :city, presence: true
   validates :price, presence: true
@@ -12,5 +12,5 @@ class Offer < ApplicationRecord
   validates :surface,presence: true
   validates :room_number, inclusion: { in: Offer::ROOM_NUMBER }
   validates :bedroom_number, inclusion: { in: Offer::BEDROOM_NUMBER }
-  validates :type, inclusion: { in: Offer::TYPE }
+  validates :good_type, inclusion: { in: Offer::GOOD_TYPE }
 end
