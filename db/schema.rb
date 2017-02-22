bf51bd168cc1f137c994a5ef2ce787d875cd2cbd
herve blasse <herve.blasse@hotmail.fr>
2017-02-22 18:00:11 +0100

    Fixed schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -41,11 +41,9 @@ ActiveRecord::Schema.define(version: 20170222133320) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.integer  "price_cents",    default: 0, null: false
-
     t.float    "latitude"
     t.float    "longitude"
-    t.index ["users_id"], name: "index_offers_on_users_id", using: :btree
-
+    t.index ["user_id"], name: "index_offers_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
