9412a3d6f1ae9a79ce76c63eb9f37fe78c944a8f
jeremycartesse <cartessejeremy@gmail.com>
2017-02-22 12:17:23 +0100

    modification of offer new and validates

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,16 @@
 class Offer < ApplicationRecord
+  ROOM_NUMBER = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+  BEDROOM_NUMBER = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+  TYPE = ["Appartment", "Floor", "House", "Building", "Commercial premises", "Parking"]
   belongs_to :user_id
+  validates :name, presence: true
+  validates :city, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
+  validates :zip_code, presence: true
+  validates :description,presence: true
+  validates :surface,presence: true
+  validates :room_number, inclusion: { in: Offer::ROOM_NUMBER }
+  validates :bedroom_number, inclusion: { in: Offer::BEDROOM_NUMBER }
+  validates :type, inclusion: { in: Offer::TYPE }
 end
